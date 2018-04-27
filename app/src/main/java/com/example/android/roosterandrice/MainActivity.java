81d491e03e5fa20ccbd753080fc95ebdcd9e76d1
AF_BA <amypyfong@gmail.com>
2018-04-26 22:50:12 -0700

    create app

---
+++ app/src/main/java/com/example/android/roosterandrice/MainActivity.java
@@ -0,0 +1,33 @@
+package com.example.android.roosterandrice;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+public class MainActivity extends AppCompatActivity implements OnMapReadyCallback {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        getSupportActionBar().hide();
+
+        setContentView(R.layout.activity_main);
+
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+    }
+
+    public void onMapReady(GoogleMap googleMap) {
+        LatLng udacity = new LatLng(37.7892335, -122.4061308);
+        googleMap.addMarker(new MarkerOptions().position(udacity).title("Rooster & Rice"));
+        googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(udacity, 15));
+    }
+}
